59851e2f0a0290263c987d107f0f72c07479de9a
Morgan Wei <115103085+1609188049@users.noreply.github.com>
2026-03-05 13:47:17 -0500

    Refactor Chess class and methods
    Refactor Chess class and its methods for better structure and clarity.

--- Chess.java
+++ Chess.java
@@ -1,4 +1,4 @@
-//Morgan Wei && Zihan Li
+//Author: Morgan(Jiageng) Wei, submitted to github on 3/5/2026
 
 package chess;
 
@@ -14,12 +14,9 @@ public class Chess {
 	 * @param move String for next move, e.g. "a2 a3"
 	 * 
 	 * @return A ReturnPlay instance that contains the result of the move.
-	 *         See the section "The Chess class" in the assignment description for details of
-	 *         the contents of the returned ReturnPlay instance.
 	 */
 	public static ReturnPlay play(String move) {
 
-		/* FILL IN THIS METHOD */
 
 
 
@@ -96,9 +93,7 @@ public class Chess {
 
 
 		
-		/* FOLLOWING LINE IS A PLACEHOLDER TO MAKE COMPILER HAPPY */
-		/* WHEN YOU FILL IN THIS METHOD, YOU NEED TO RETURN A ReturnPlay OBJECT */
-		// return null;
+
 	}
 	
 	
@@ -574,4 +569,5 @@ class Board {
             }
         }
     }
+
 }
